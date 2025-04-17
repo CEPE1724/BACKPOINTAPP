e2833df7c067887bf34d641be0a4075c663cda7f
stalin21r <maick_rony@epn.edu.ec>
2025-04-17 17:36:53 -0500

    avance añadir productos al carrito ¡NO HACER MERGE A LA MAIN

--- Lia/productos/controller.js
+++ Lia/productos/controller.js
@@ -1,196 +1,269 @@
-const { AppDataSource } = require("../../ApiCobrador/api/config/database");
-const { body, validationResult } = require('express-validator');
+/* eslint-disable camelcase */
+const { AppDataSource } = require('../../ApiCobrador/api/config/database')
+const { body, validationResult } = require('express-validator')
+const { v4: uuidv4 } = require('uuid')
 
-require('dotenv').config({ path: '../../.env' });  // Cargar las variables de entorno desde el archivo .env
+require('dotenv').config({ path: '../../.env' }) // Cargar las variables de entorno desde el archivo .env
 // Función para generar la URL amigable
-
 
 // Controlador
 function formatUrl(Grupo, SubGrupo, nombreComercial, sku) {
-    const formatString = (str) =>
-        str
-            .toLowerCase()
-            .normalize('NFD') // Normaliza caracteres acentuados
-            .replace(/[\u0300-\u036f]/g, '') // Elimina diacríticos
-            .replace(/[^\w\s-]/g, '') // Elimina caracteres especiales excepto guiones
-            .trim()
-            .replace(/\s+/g, '-'); // Reemplaza espacios con guiones
+  uuidv4()
+  const formatString = (str) =>
+    str
+      .toLowerCase()
+      .normalize('NFD') // Normaliza caracteres acentuados
+      .replace(/[\u0300-\u036f]/g, '') // Elimina diacríticos
+      .replace(/[^\w\s-]/g, '') // Elimina caracteres especiales excepto guiones
+      .trim()
+      .replace(/\s+/g, '-') // Reemplaza espacios con guiones
 
-    const urlGrupo = formatString(Grupo);
-    const urlSubGrupo = formatString(SubGrupo);
-    const urlNombre = formatString(nombreComercial);
+  const urlGrupo = formatString(Grupo)
+  const urlSubGrupo = formatString(SubGrupo)
+  const urlNombre = formatString(nombreComercial)
 
-    return `${urlGrupo}-${urlSubGrupo}-${urlNombre}-${sku}`;
+  return `${urlGrupo}-${urlSubGrupo}-${urlNombre}-${sku}`
 }
 
 // Controlador principal
 exports.Baratazos_WEB_WP = async (req, res) => {
-    try {
-        const result = await AppDataSource.query(`EXEC dbo.WEB_Baratazos_SP`);
+  try {
+    const result = await AppDataSource.query('EXEC dbo.WEB_Baratazos_SP')
 
-        if (!result || result.length === 0) {
-            return res.status(404).json({
-                status: 'error',
-                message: 'No se encontraron baratazos',
-                data: null,
-                totalRecords: 0,
-            });
-        }
-
-        const marketplaceUrl = process.env.MARKETPLACE_URL || 'https://www.point.com.ec/'; // URL base del marketplace
-
-        const data = result.map((p) => {
-            const {
-                Codigo,
-                Titulo,
-                Descripcion,
-                Imagen,
-                PrecioAntes,
-                PrecioPromocional,
-                Grupo,
-                SubGrupo,
-                idWEB_Baratazos,
-            } = p;
-
-            const Url = `${marketplaceUrl}productobaratazo/` +
-                formatUrl(Grupo || '', SubGrupo || '', Titulo || '', idWEB_Baratazos || 0);
-
-            return {
-                Codigo,
-                Titulo,
-                Descripcion,
-                PrecioAntes,
-                PrecioPromocional,
-                Imagen,
-                Url,
-            };
-        });
-
-        return res.status(200).json({
-            status: 'success',
-            message: 'Baratazos obtenidos.',
-            data,
-            totalRecords: data.length,
-        });
-    } catch (err) {
-        console.error("Error al ejecutar el procedimiento almacenado:", err);
-        return res.status(500).json({
-            status: 'error',
-            message: 'Error interno del servidor',
-            data: null,
-            totalRecords: 0,
-        });
+    if (!result || result.length === 0) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'No se encontraron baratazos',
+        data: null,
+        totalRecords: 0
+      })
     }
+
+    const marketplaceUrl =
+      process.env.MARKETPLACE_URL || 'https://www.point.com.ec/' // URL base del marketplace
+
+    const data = result.map((p) => {
+      const {
+        Codigo,
+        Titulo,
+        Descripcion,
+        Imagen,
+        PrecioAntes,
+        PrecioPromocional,
+        Grupo,
+        SubGrupo,
+        idWEB_Baratazos
+      } = p
+
+      const Url =
+        `${marketplaceUrl}productobaratazo/` +
+        formatUrl(
+          Grupo || '',
+          SubGrupo || '',
+          Titulo || '',
+          idWEB_Baratazos || 0
+        )
+
+      return {
+        Codigo,
+        Titulo,
+        Descripcion,
+        PrecioAntes,
+        PrecioPromocional,
+        Imagen,
+        Url
+      }
+    })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Baratazos obtenidos.',
+      data,
+      totalRecords: data.length
+    })
+  } catch (err) {
+    console.error('Error al ejecutar el procedimiento almacenado:', err)
+    return res.status(500).json({
+      status: 'error',
+      message: 'Error interno del servidor',
+      data: null,
+      totalRecords: 0
+    })
+  }
 }
 
 exports.Ofertas_WEB_WP = async (req, res) => {
-    try {
-        const result = await AppDataSource.query(`EXEC dbo.WEB_Ofertas_SP`);
+  try {
+    const result = await AppDataSource.query('EXEC dbo.WEB_Ofertas_SP')
 
-        if (!result || result.length === 0) {
-            return res.status(404).json({
-                status: 'error',
-                message: 'No se encontraron baratazos',
-                data: null,
-                totalRecords: 0,
-            });
-        }
-
-        const marketplaceUrl = process.env.MARKETPLACE_URL || 'https://www.point.com.ec/'; // URL base del marketplace
-
-        const data = result.map((p) => {
-            const {
-                Titulo,
-                Descripcion,
-                Imagen,
-                PrecioTarjeta,
-                Grupo,
-                SubGrupo,
-                idWEB_Ofertas,
-            } = p;
-
-            const Url = `${marketplaceUrl}productooferta/` +
-                formatUrl(Grupo || '', SubGrupo || '', Titulo || '', idWEB_Ofertas || 0);
-
-            return {
-                Titulo,
-                Descripcion,
-                Precio: PrecioTarjeta,
-                Imagen,
-                Url,
-            };
-        });
-
-        return res.status(200).json({
-            status: 'success',
-            message: 'Ofertas obtenidas.',
-            data,
-            totalRecords: data.length,
-        });
-    } catch (err) {
-        console.error("Error al ejecutar el procedimiento almacenado:", err);
-        return res.status(500).json({
-            status: 'error',
-            message: 'Error interno del servidor',
-            data: null,
-            totalRecords: 0,
-        });
+    if (!result || result.length === 0) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'No se encontraron baratazos',
+        data: null,
+        totalRecords: 0
+      })
     }
+
+    const marketplaceUrl =
+      process.env.MARKETPLACE_URL || 'https://www.point.com.ec/' // URL base del marketplace
+
+    const data = result.map((p) => {
+      const {
+        Titulo,
+        Descripcion,
+        Imagen,
+        PrecioTarjeta,
+        Grupo,
+        SubGrupo,
+        idWEB_Ofertas
+      } = p
+
+      const Url =
+        `${marketplaceUrl}productooferta/` +
+        formatUrl(Grupo || '', SubGrupo || '', Titulo || '', idWEB_Ofertas || 0)
+
+      return {
+        Titulo,
+        Descripcion,
+        Precio: PrecioTarjeta,
+        Imagen,
+        Url
+      }
+    })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Ofertas obtenidas.',
+      data,
+      totalRecords: data.length
+    })
+  } catch (err) {
+    console.error('Error al ejecutar el procedimiento almacenado:', err)
+    return res.status(500).json({
+      status: 'error',
+      message: 'Error interno del servidor',
+      data: null,
+      totalRecords: 0
+    })
+  }
 }
 
 exports.productosWEB_WP = async (req, res) => {
-    try {
-        const result = await AppDataSource.query(`EXEC dbo.WEB_Productos_SP_Lia`);
+  try {
+    const result = await AppDataSource.query('EXEC dbo.WEB_Productos_SP_Lia')
 
-        if (!result || result.length === 0) {
-            return res.status(404).json({
-                status: 'error',
-                message: 'No se encontraron baratazos',
-                data: null,
-                totalRecords: 0,
-            });
-        }
-
-        const marketplaceUrl = process.env.MARKETPLACE_URL || 'https://www.point.com.ec/'; // URL base del marketplace
-
-        const data = result.map((p) => {
-            const {
-                Codigo,
-                Titulo,
-                Descripcion,
-                Imagen,
-                Tarjeta,
-                Grupo,
-                Subgrupo,
-                sku,
-            } = p;
-
-            const Url = `${marketplaceUrl}producto/` +
-                formatUrl(Grupo || '', Subgrupo || '', Titulo || '', sku || 0);
-
-            return {
-                Codigo,
-                Titulo,
-                Descripcion,
-                Precio: Tarjeta,
-                Imagen,
-                Url,
-            };
-        });
-
-        return res.status(200).json({
-            status: 'success',
-            message: 'Productos obtenidos.',
-            data,
-            totalRecords: data.length,
-        });
-    } catch (err) {
-        console.error("Error al ejecutar el procedimiento almacenado:", err);
-        return res.status(500).json({
-            status: 'error',
-            message: 'Error interno del servidor',
-            data: null,
-            totalRecords: 0,
-        });
+    if (!result || result.length === 0) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'No se encontraron baratazos',
+        data: null,
+        totalRecords: 0
+      })
     }
+
+    const marketplaceUrl =
+      process.env.MARKETPLACE_URL || 'https://www.point.com.ec/' // URL base del marketplace
+
+    const data = result.map((p) => {
+      const {
+        Codigo,
+        Titulo,
+        Descripcion,
+        Imagen,
+        Tarjeta,
+        Grupo,
+        Subgrupo,
+        sku
+      } = p
+
+      const Url =
+        `${marketplaceUrl}producto/` +
+        formatUrl(Grupo || '', Subgrupo || '', Titulo || '', sku || 0)
+
+      return {
+        Codigo,
+        Titulo,
+        Descripcion,
+        Precio: Tarjeta,
+        Imagen,
+        Url
+      }
+    })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Productos obtenidos.',
+      data,
+      totalRecords: data.length
+    })
+  } catch (err) {
+    console.error('Error al ejecutar el procedimiento almacenado:', err)
+    return res.status(500).json({
+      status: 'error',
+      message: 'Error interno del servidor',
+      data: null,
+      totalRecords: 0
+    })
+  }
+}
+
+exports.crearCarrito = async (req, res) => {
+  let {
+    idWEB_Carrito,
+    KeyCarrito,
+    MetodoPago,
+    idWEB_Clientes,
+    carritoDetalle
+  } = req.body
+
+  let {
+    idWEB_CarritoDetalle,
+    idWEB_Categorias,
+    Nombre,
+    Id,
+    idWEB_Carrito,
+    KeyCarrito,
+    Cantidad,
+    MetodoPago,
+    Cuotas,
+    ValorCuota,
+    PriceCard,
+    KeyItemCarrito,
+    Imagen,
+    idWEB_Clientes,
+    carritoDetalleSelIntangibleDto,
+    idWEB_Grupo,
+    PrecioCreditoDirecto } = carritoDetalle
+
+  try {
+    if (!idWEB_Carrito || idWEB_Carrito <= 0) {
+      KeyCarrito = uuidv4()
+    } else {
+      const findKeyCarrito = await this.AppDataSource.query(
+        'select wc.keyCarrito from WEB_Carrito wc where wc.idWEB_Carrito = @0',
+        [idWEB_Carrito]
+      )
+      if (!KeyCarrito || KeyCarrito.length === 0) {
+        return res.status(404).json({
+          status: 'error',
+          message: 'No se encontró el carrito',
+          data: null,
+          totalRecords: 0
+        })
+      }
+      KeyCarrito = findKeyCarrito[0].keyCarrito
+    }
+
+    const detalleExistente = await this.AppDataSource.query(
+      'select * from WEB_CarritoDetalle where idWEB_Categorias = @0 and Id = @1 and idWEB_Grupo = @2'
+    )
+  } catch (err) {
+    return res.status(500).json({
+      status: 'error',
+      message: 'Error interno del servidor',
+      data: null,
+      totalRecords: 0
+    })
+  }
 }
--- Lia/productos/token.js
+++ Lia/productos/token.js
@@ -1,24 +1,22 @@
+require('dotenv').config({ path: '../../.env' }) // Cargar las variables de entorno
 
-
-require('dotenv').config({path:'../../.env'}); // Cargar las variables de entorno
-
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 // Información del cliente (esto podría venir de tu base de datos o de otro lugar)
 const cliente = {
-    clienteId: 4,
-    nombre: 'LiaGestionesPoint$.$',
-    correo: 'dpozo@point.com.ec',
-    roles: ['usuario', 'Lia'],  // Ejemplo de roles
-};
+  clienteId: 4,
+  nombre: 'LiaGestionesPoint$.$',
+  correo: 'dpozo@point.com.ec',
+  roles: ['usuario', 'Lia'] // Ejemplo de roles
+}
 // Leer la clave secreta desde las variables de entorno
-const secretKey = process.env.FIXED_TOKEN_LIA;  // Cambié SECRET_KEY por FIXED_TOKEN_WHATSAPP
+const secretKey = process.env.FIXED_TOKEN_LIA // Cambié SECRET_KEY por FIXED_TOKEN_WHATSAPP
 
 if (!secretKey) {
-    console.error('Error: La clave secreta no está definida en el archivo .env');
-    process.exit(1);  // Detener la ejecución si no se encuentra la clave secreta
+  console.error('Error: La clave secreta no está definida en el archivo .env')
+  process.exit(1) // Detener la ejecución si no se encuentra la clave secreta
 }
 
 // Crear un token estático con la información del cliente
-const token = jwt.sign(cliente, secretKey, { expiresIn: '365d' });  // El token durará 1 año
+const token = jwt.sign(cliente, secretKey, { expiresIn: '365d' }) // El token durará 1 año
 
-console.log('Token generado:', token);
+console.log('Token generado:', token)
