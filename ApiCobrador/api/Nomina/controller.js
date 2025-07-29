1132728e1a5382385f07e50452a0ef1b8e36c8a8
CEPE1724 <edisonnacional1@hotmail.com>
2025-07-28 21:26:23 -0500

    actualiza codigo nomina

--- ApiCobrador/api/Nomina/controller.js
+++ ApiCobrador/api/Nomina/controller.js
@@ -1,14 +1,35 @@
 
 const { AppDataSource } = require("../config/database");
 const Nomina = require('./model'); // Asegúrate de que 'Nomina' esté bien importado
-
+const DispositivosAPP = require('../DispositivosAPP/model'); // Asegúrate de que 'DispositivosAPP' esté bien importado
 exports.getProductoSolicitudByIdNomina = async (req, res) => {
     const { idnomina } = req.params;
 
     try {
+       // obtener usuaropapp
+        const dispositivoRepository = AppDataSource.getRepository(DispositivosAPP);
+        const dispositivo = await dispositivoRepository.findOne({
+            where: { idNomina: idnomina },
+        });
+        if (!dispositivo) {
+            return res.status(404).json({
+                success: false,
+                message: 'Dispositivo not found'
+            });
+        }
+        // obtener Codigo 
+        const UsuarioAPP = dispositivo.UsuarioAPP;
+        if (!UsuarioAPP) {
+            return res.status(404).json({
+                success: false,
+                message: 'UsuarioAPP not found'
+            });
+        }
+
+
         const nominaRepository = AppDataSource.getRepository(Nomina);
         const nomina = await nominaRepository.findOne({
-            where: { idNomina: idnomina },
+            where: { Codigo: UsuarioAPP },
         });
 
         if (!nomina) {
