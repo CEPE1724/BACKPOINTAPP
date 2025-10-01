b5fff1dee739ea2b919d9d2dae7faafba17ae24e
CEPE1724 <edisonnacional1@hotmail.com>
2025-10-01 16:04:45 -0500

    api dt

--- ApiCobrador/api/googleApi/router.js
+++ ApiCobrador/api/googleApi/router.js
@@ -5,4 +5,5 @@ const googleController = require('./controller');
 // Ruta para subir un archivo a Google Cloud Storage
 router.put('/googleApi/google', googleController.upload.single('file') , googleController.uploadFileToGCS);
 router.put('/googleApi/google/Latinium', googleController.upload.single('file') , googleController.uploadFileToGCSLatinium);
+router.get('/googleApi/google/DepartamentoTecnico/:idIngreso/:Ingreso', googleController.getFilesFromGCSDepartamentoTecnico);
 module.exports = router;
