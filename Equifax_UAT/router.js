cd46f7d6b7c93fd73db89bd58d0b74d6d0357319
CEPE1724 <edisonnacional1@hotmail.com>
2025-11-26 12:48:50 -0500

    actualizacion .env

--- Equifax_UAT/router.js
+++ Equifax_UAT/router.js
@@ -6,6 +6,7 @@ const controller = require('./controller');
 const validateToken = require('./Token/auth');  // Asegúrate de importar el middleware
 
 // Ruta protegida por el token
+console.log('Setting up /oauth route with token validation middleware');
 router.post('/oauth', validateToken, controller.equifaxOauth);
 module.exports = router;
 
